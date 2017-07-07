14eef4ad19753d73fc251b58d2a0b94995d027ff
KAJATIGER <ksantro@gmx.de>
2017-07-07 14:03:56 +0200

    uploader

---
+++ uploadfolder.rb
@@ -0,0 +1,23 @@
+require "google_drive"
+
+session = GoogleDrive::Session.from_config("client_secret.json")
+#create folder
+puts "Name your folder:"
+folder_name = gets.chomp
+folder = session.root_collection.create_subcollection(folder_name)
+# create file
+file_name = Time.new
+
+puts "select a file by typing the name of one from this list"
+
+basedir = 'uploads'
+
+files = Dir.glob("*")
+
+puts files
+
+selection = gets.chomp
+
+file = session.upload_from_file(selection, file_name, convert: false)
+#put file into folder
+folder.add(file)
