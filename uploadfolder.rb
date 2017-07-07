7f5362f0ac32d7d27de7fad1f4a961a9ec4be607
Jen <jvarelalijo@gmail.com>
2017-07-07 14:35:17 +0200

    add find folder

--- uploadfolder.rb
+++ uploadfolder.rb
@@ -1,23 +1,25 @@
 require "google_drive"
 
 session = GoogleDrive::Session.from_config("client_secret.json")
-#create folder
-puts "Name your folder:"
-folder_name = gets.chomp
-folder = session.root_collection.create_subcollection(folder_name)
 # create file
-file_name = Time.new
-
 puts "select a file by typing the name of one from this list"
 
-basedir = 'uploads'
-
 files = Dir.glob("*")
-
 puts files
 
 selection = gets.chomp
+file_name = Time.new
 
 file = session.upload_from_file(selection, file_name, convert: false)
 #put file into folder
-folder.add(file)
+puts "Name your folder:"
+folder_name = gets.chomp
+
+existent_folders = session.root_collection.subcollections
+found = session.root_collection.subcollection_by_title(folder_name)
+
+if found
+  found.add(file)
+else
+  folder = session.root_collection.create_subcollection(folder_name).add(file)
+end
